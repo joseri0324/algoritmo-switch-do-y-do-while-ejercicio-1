9ffc431021d5788cdc6ae2a8797d018dec5e5ccf
jose <josesaa0324@gmail.com>
2024-11-04 19:44:14 -0500

    se realiza el archivo js. con su formula y su respuesta.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+let nombre = prompt("Ingresar el nombre");
+
+let estado = parseInt(prompt("Seleccione su estado civil (1: Soltero, 2: Casado, 3: Separado, 4: Viudo, 5: Unión libre):"));
+
+
+switch (estado) {
+  case 1:
+    alert(nombre + ", " + "soltero");
+    break;
+  case 2:
+    alert(nombre + ", " + "casado");
+    break;
+  case 3:
+    alert(nombre + ", " + "separado");
+    break;
+  case 4:
+    alert(nombre + ", " + "viudo");
+    break;
+  case 5:
+    alert(nombre + ", " + "union libre");
+    break;
+  default:
+    alert(nombre + ", " + "codigo invalido");
+    break;
+}
+
 
 
 
